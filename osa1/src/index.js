e5166e575c3ec08ed155d163beedff7dd25dc945
Heikki Holopainen <heikki.holopainen@gmail.com>
2018-01-20 13:45:02 +0200

    exercise 1.3 and add ignore file

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+    const kurssi = 'Half Stack -sovelluskehitys';
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    };
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    };
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    };
 
     return (
         <div>
             <Otsikko kurssi={kurssi}/>
-            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2}
-                     tehtavia3={tehtavia3}/>
-            <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3}/>
+            <Yhteensa osa1={osa1} osa2={osa2} osa3={osa3}/>
         </div>
     )
 };
-
 
 const Osa = (props) => {
     return (
@@ -27,14 +32,14 @@ const Osa = (props) => {
             <p>{props.osa} {props.tehtavia}</p>
         </div>
     )
-}
+};
 
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
-            <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
-            <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+            <Osa osa={props.osa1['nimi']} tehtavia={props.osa1['tehtavia']} />
+            <Osa osa={props.osa2['nimi']} tehtavia={props.osa2['tehtavia']} />
+            <Osa osa={props.osa3['nimi']} tehtavia={props.osa3['tehtavia']} />
         </div>
     )
 };
@@ -53,7 +58,6 @@ const Yhteensa = (props) => {
             <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
         </div>
     )
-
 };
 
 ReactDOM.render(
