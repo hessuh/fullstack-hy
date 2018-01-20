a7dba32408336a9f3acb1f0c3b01f2c6a2b0c4b6
Heikki Holopainen <heikki.holopainen@gmail.com>
2018-01-20 14:13:03 +0200

    Exercise 1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,27 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys';
-    const osat = [
-        {
-            nimi: 'Reactin perusteet',
-            tehtavia: 10
-        },
-        {
-            nimi: 'Tiedonvälitys propseilla',
-            tehtavia: 7
-        },
-        {
-            nimi: 'Komponenttien tila',
-            tehtavia: 14
-        }
-    ];
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14
+            }
+        ]
+    };
 
     return (
         <div>
             <Otsikko kurssi={kurssi}/>
-            <Sisalto osat={osat} />
-            <Yhteensa osat={osat}/>
+            <Sisalto kurssi={kurssi}/>
+            <Yhteensa kurssi={kurssi}/>
         </div>
     )
 };
@@ -38,9 +40,9 @@ const Osa = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <Osa osa={props.osat[0]} />
-            <Osa osa={props.osat[1]} />
-            <Osa osa={props.osat[2]} />
+            <Osa osa={props.kurssi.osat[0]}/>
+            <Osa osa={props.kurssi.osat[1]}/>
+            <Osa osa={props.kurssi.osat[2]}/>
         </div>
     )
 };
@@ -48,7 +50,7 @@ const Sisalto = (props) => {
 const Otsikko = (props) => {
     return (
         <div>
-            <h1>{props.kurssi}</h1>
+            <h1>{props.kurssi.nimi}</h1>
         </div>
     )
 };
@@ -56,7 +58,7 @@ const Otsikko = (props) => {
 const Yhteensa = (props) => {
     return (
         <div>
-            <p>yhteensä {props.osat[0]['tehtavia'] + props.osat[1]['tehtavia'] + props.osat[2]['tehtavia']} tehtävää</p>
+            <p>yhteensä {props.kurssi.osat[0]['tehtavia'] + props.kurssi.osat[1]['tehtavia'] + props.kurssi.osat[2]['tehtavia']} tehtävää</p>
         </div>
     )
 };
