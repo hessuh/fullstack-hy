e98627b1a5bf781d873a68a22ac4250140e626b2
Heikki Holopainen <heikki.holopainen@gmail.com>
2018-01-21 20:29:59 +0200

    Exercise 1.6 unicafe part 1

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,8 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            positive: 0,
+            neutral: 0,
+            bad : 0
+        }
+    }
+
+    ClickPositive= () => {
+        this.setState({
+            positive: this.state.positive + 1
+        })
+    };
+
+    ClickNeutral = () => {
+        this.setState({
+            neutral: this.state.neutral + 1
+        })
+    };
+
+    ClickBad = () => {
+        this.setState({
+            bad: this.state.bad + 1
+        })
+    };
+
+
+    GiveFeedback = () => {
+        return (
+            <div>
+                <p> Anna palautetta </p>
+                <button onClick={this.ClickPositive}>Hyvä</button>
+                <button onClick={this.ClickNeutral}>Neutraali</button>
+                <button onClick={this.ClickBad}>Huono</button>
+            </div>
+        )
+    };
+
+    Statistics = () => {
+        return (
+            <div>
+                <p> Statistiikka </p>
+                <div>
+                    Hyvä {this.state.positive}
+                </div>
+                <div>
+                    Neutraali {this.state.neutral}
+                </div>
+                <div>
+                    Huono {this.state.bad}
+                </div>
+
+            </div>
+        )
+    };
+
+    render() {
+        return (
+            <div>
+                <this.GiveFeedback/>
+                <this.Statistics/>
+            </div>
+        )
+    }
+
+};
+
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
