56273621908989f20e896afa164a88e96695c20e
Heikki Holopainen <heikki.holopainen@gmail.com>
2018-02-04 20:06:56 +0200

    Separate components into modules

---
+++ osa2/komponentit/src/komponentit/App.js
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const Osa = ({osa}) => <p>{osa.nimi} {osa.tehtavia}</p>
+const Otsikko = ({otsikko}) => <h1>{otsikko}</h1>
+const Sisalto = ({osat}) => <div>{osat.map(osa => <Osa osa={osa} key={osa.id}/>)}</div>
+const Yhteensa = ({osat}) => <p>yhteensä {sum(osat)} tehtävää</p>
+
+const sum = (osat) => osat.reduce((yht, iter) => yht + iter.tehtavia, 0)
+
+
+const Kurssi = ({kurssi}) => {
+    return (
+        <div>
+            <Otsikko nimi={kurssi.nimi}/>
+            <Sisalto osat={kurssi.osat}/>
+            <Yhteensa osat={kurssi.osat}/>
+        </div>
+    )
+}
+
+const App = () => {
+    const kurssit = [
+        {
+            nimi: 'Half Stack -sovelluskehitys',
+            osat: [
+                {
+                    nimi: 'Reactin perusteet',
+                    tehtavia: 10,
+                    id: 1
+                },
+                {
+                    nimi: 'Tiedonvälitys propseilla',
+                    tehtavia: 7,
+                    id: 2
+                },
+                {
+                    nimi: 'Komponenttien tila',
+                    tehtavia: 14,
+                    id: 3
+                },
+            ]
+        },
+        {
+            nimi: 'Node.js',
+            id: 2,
+            osat: [
+                {
+                    nimi: 'Routing',
+                    tehtavia: 3,
+                    id: 1
+                },
+                {
+                    nimi: 'Middlewaret',
+                    tehtavia: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+    return (
+        <div>
+            <h1>Half-Stack -sovelluskehitys</h1>
+
+            {kurssit.map(kurssi => <Kurssi kurssi={kurssi} key={kurssi.id}/>)}
+        </div>
+    )
+}
+
+export default App;
