c38ae200edf3dd9d4e9c7c966fd3ed425eb63ade
Heikki Holopainen <heikki.holopainen@gmail.com>
2018-02-04 20:10:57 +0200

    Extract kurssi into own module

--- osa2/komponentit/src/komponentit/App.js
+++ osa2/komponentit/src/komponentit/App.js
@@ -1,22 +1,5 @@
 import React from 'react'
-
-const Osa = ({osa}) => <p>{osa.nimi} {osa.tehtavia}</p>
-const Otsikko = ({otsikko}) => <h1>{otsikko}</h1>
-const Sisalto = ({osat}) => <div>{osat.map(osa => <Osa osa={osa} key={osa.id}/>)}</div>
-const Yhteensa = ({osat}) => <p>yhteensä {sum(osat)} tehtävää</p>
-
-const sum = (osat) => osat.reduce((yht, iter) => yht + iter.tehtavia, 0)
-
-
-const Kurssi = ({kurssi}) => {
-    return (
-        <div>
-            <Otsikko nimi={kurssi.nimi}/>
-            <Sisalto osat={kurssi.osat}/>
-            <Yhteensa osat={kurssi.osat}/>
-        </div>
-    )
-}
+import Kurssi from './Kurssi'
 
 const App = () => {
     const kurssit = [
