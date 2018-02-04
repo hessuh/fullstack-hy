cbfa175160cbde9a10a6e1c750dd6f392d005c03
Heikki Holopainen <heikki.holopainen@gmail.com>
2018-02-04 20:00:11 +0200

    Exercise 2.1 - 2.4

--- osa2/komponentit/src/index.js
+++ osa2/komponentit/src/index.js
@@ -1,54 +1,75 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
-const Sisalto = (props) => {
-    const [osa1, osa2, osa3] = props.kurssi.osat
-    return(
-        <div>
-        <Osa osa={osa1.nimi} tehtavia={osa1.tehtavia} />
-    <Osa osa={osa2.nimi} tehtavia={osa2.tehtavia} />
-    <Osa osa={osa3.nimi} tehtavia={osa3.tehtavia} />
-    </div>
-)
-}
-const Yhteensa = (props) => {
-    const [osa1, osa2, osa3] = props.kurssi.osat
 
-    return(
-        <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
-)
+const Osa = ({osa}) => <p>{osa.nimi} {osa.tehtavia}</p>
+const Otsikko = ({otsikko}) => <h1>{otsikko}</h1>
+const Sisalto = ({osat}) => <div>{osat.map(osa => <Osa osa={osa} key={osa.id}/>)}</div>
+const Yhteensa = ({osat}) => <p>yhteensä {sum(osat)} tehtävää</p>
+
+const sum = (osat) => osat.reduce((yht, iter) => yht + iter.tehtavia, 0)
+
+
+const Kurssi = ({kurssi}) => {
+    return (
+        <div>
+            <Otsikko nimi={kurssi.nimi}/>
+            <Sisalto osat={kurssi.osat}/>
+            <Yhteensa osat={kurssi.osat}/>
+        </div>
+    )
 }
 
 const App = () => {
-    const kurssi = {
-        nimi: 'Half Stack -sovelluskehitys',
-        osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14
-            }
-        ]
-    }
+    const kurssit = [
+        {
+            nimi: 'Half Stack -sovelluskehitys',
+            osat: [
+                {
+                    nimi: 'Reactin perusteet',
+                    tehtavia: 10,
+                    id: 1
+                },
+                {
+                    nimi: 'Tiedonvälitys propseilla',
+                    tehtavia: 7,
+                    id: 2
+                },
+                {
+                    nimi: 'Komponenttien tila',
+                    tehtavia: 14,
+                    id: 3
+                },
+            ]
+        },
+        {
+            nimi: 'Node.js',
+            id: 2,
+            osat: [
+                {
+                    nimi: 'Routing',
+                    tehtavia: 3,
+                    id: 1
+                },
+                {
+                    nimi: 'Middlewaret',
+                    tehtavia: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
     return (
         <div>
-        <Otsikko kurssi={kurssi}/>
-    <Sisalto kurssi={kurssi} />
-    <Yhteensa kurssi={kurssi}  />
-    </div>
-)
+            <h1>Half-Stack -sovelluskehitys</h1>
+
+            {kurssit.map(kurssi => <Kurssi kurssi={kurssi} key={kurssi.id}/>)}
+        </div>
+    )
 }
 
 ReactDOM.render(
-<App />,
+    <App/>,
     document.getElementById('root')
 )
